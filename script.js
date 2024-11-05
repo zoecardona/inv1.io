80531b221447b4ba0b23e2ae722899db6e52cbf3
zoecardona <zoecardonamaldonado@gmail.com>
2024-11-04 23:13:50 -0600

    ultima modificacion

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const countdownDate = new Date("Jan 24, 2025 17:00:00").getTime(); // Inicio de la cuenta regresiva
 
+function padNumber(number) {
+    return number < 10 ? '0' + number : number;
+}
+
 const x = setInterval(function() {
     const now = new Date().getTime(); // Tiempo actual
     const distance = countdownDate - now; // Diferencia entre la fecha actual y la fecha del evento
@@ -10,11 +14,11 @@ const x = setInterval(function() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Mostrar resultado en los elementos HTML
-    document.getElementById("days").innerHTML = days + " :";
-    document.getElementById("hours").innerHTML = hours + " :";
-    document.getElementById("minutes").innerHTML = minutes + " :";
-    document.getElementById("seconds").innerHTML = seconds + "";
+    // Mostrar resultado en los elementos HTML con ceros a la izquierda
+    document.getElementById("days").innerHTML = padNumber(days) + " :";
+    document.getElementById("hours").innerHTML = padNumber(hours) + " :";
+    document.getElementById("minutes").innerHTML = padNumber(minutes) + " :";
+    document.getElementById("seconds").innerHTML = padNumber(seconds);
 
     // Si el contador ha llegado a cero
     if (distance < 0) {
